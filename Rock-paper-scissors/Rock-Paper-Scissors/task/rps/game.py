8ce16b61494429c8f5cc26991c93bdf9f698d480
xxxAgaxxx <37577999+xxxAgaxxx@users.noreply.github.com>
2019-09-13 23:29:40 +0700

    RPS stage 3 try

--- Rock-paper-scissors/Rock-Paper-Scissors/task/rps/game.py
+++ Rock-paper-scissors/Rock-Paper-Scissors/task/rps/game.py
@@ -1,7 +1,6 @@
 import random
 
-
-def get_shape():
+def players_decision():
     shape = input()
     return shape
 
@@ -15,15 +14,31 @@ def round_result(player, computer):
     if player == computer:
         print('There is a draw ({})'.format(player))
         return 0
-    elif (player == 'rock' and computer == 'paper') or (player == 'paper' and computer == 'scissors') or (player == 'scissors' and computer == 'rock'):
+    elif (player == 'rock' and computer == 'paper') or \
+            (player == 'paper' and computer == 'scissors') or \
+            (player == 'scissors' and computer == 'rock'):
         print('Sorry, but computer chose ' + computer)
         return 1
-    else:
+    elif (player == 'paper' and computer == 'rock') or \
+            (player == 'scissors' and computer == 'paper') or \
+            (player == 'rock' and computer == 'scissors'):
         print('Well done. Computer chose {} and failed'.format(computer))
-        return 'rock'
+        return 2
+    else:
+        return 3
 
 
-shape_pool = ['rock', 'paper', 'sciccors']
-players_shape = get_shape()
-computers_shape = computers_decision(shape_pool)
-round_result(players_shape, computers_shape)
+shape_pool = ['rock', 'paper', 'scissors']
+while True:
+    players_shape = players_decision()
+    if players_shape == '!exit':
+        break
+    if players_shape == '!help':
+        print('''Please type "rock", "paper" or "scissors"
+        You win if your shape beats computer's shape:
+        Paper>rock>scissors>paper and so on''')
+        continue
+    computers_shape = computers_decision(shape_pool)
+    while round_result(players_shape,computers_shape) == '3':
+        continue
+print('Bye!')
