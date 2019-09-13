5bb20fb698a07df6fdb64b95df29ae36fd422666
xxxAgaxxx <37577999+xxxAgaxxx@users.noreply.github.com>
2019-09-13 17:57:45 +0700

    RPS stage 2

--- Rock-paper-scissors/Rock-Paper-Scissors/task/rps/game.py
+++ Rock-paper-scissors/Rock-Paper-Scissors/task/rps/game.py
@@ -1,16 +1,29 @@
+import random
+
+
 def get_shape():
     shape = input()
     return shape
 
 
-def round_result(player):
-    if player == 'rock':
-        return 'paper'
-    if player == 'paper':
-        return 'scissors'
-    if player == 'scissors':
+def computers_decision(shapes):
+    computers_shape = random.choice(shapes)
+    return computers_shape
+
+
+def round_result(player, computer):
+    if player == computer:
+        print('There is a draw ({})'.format(player))
+        return 0
+    elif (player == 'rock' and computer == 'paper') or (player == 'paper' and computer == 'scissors') or (player == 'scissors' and computer == 'rock'):
+        print('Sorry, but computer chose ' + computer)
+        return 1
+    else:
+        print('Well done. Computer chose {} and failed'.format(computer))
         return 'rock'
 
 
+shape_pool = ['rock', 'paper', 'sciccors']
 players_shape = get_shape()
-print('Sorry, but computer chose ' + round_result(players_shape))
+computers_shape = computers_decision(shape_pool)
+round_result(players_shape, computers_shape)
