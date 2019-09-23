6068f0b2496c9c4d827ef5f8a20c5baf17973de3
r.bykov <r.bykov@movavi.com>
2019-09-23 10:52:10 +0700

    tic-tac-toe stage 1 correction

--- Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
@@ -1,11 +1,18 @@
 def show_field(field):
     counter = 0
-    for row in field:
-        print(row)
+    for symbol in field:
+        print(symbol, end='')
+        while counter < 2:
+            print(' ', end='')
+            counter += 1
+            break
+        else:
+            print('')
+            counter = 0
 
 
-initial_field = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+initial_field = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-current_field = [['X', 'O', 'X'], ['O', 'X', 'O'], ['X', 'O', 'X']]
+current_field = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 
 show_field(current_field)
