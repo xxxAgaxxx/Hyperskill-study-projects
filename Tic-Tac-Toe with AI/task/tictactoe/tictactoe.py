a1168f133b337215053e6c5375b350b24d8389cf
Roman Bykov <r.bykov@movavi.com>
2019-09-30 17:50:38 +0700

    Update tictactoe.py
    Tic-Tac-Toe stage2

--- Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
@@ -1,18 +1,15 @@
+def initiate_field():
+    field_string = input('Enter cells: ')
+    cell_states = [field_string[i:i+1] for i in range(1, len(field_string) - 1, 1)]
+    return cell_states
+
+
 def show_field(field):
-    counter = 0
-    for symbol in field:
-        print(symbol, end='')
-        while counter < 2:
-            print(' ', end='')
-            counter += 1
-            break
-        else:
-            print('')
-            counter = 0
+    print('---------')
+    for i in range(3):
+        print(f'| {field[3 * i]} {field[3 * i + 1]} {field[3 * i + 2]} |')
+    print('---------')
 
 
-initial_field = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-
-current_field = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
-
+current_field = initiate_field()
 show_field(current_field)
