240d3e1d2a5458d0675d374b40aeb8f862850104
RomaAga <bromaaga@gmail.com>
2019-11-11 22:10:06 +0700

    rps wip
    reworked logic
    add reading from rating file

--- Rock-paper-scissors/game.py
+++ Rock-paper-scissors/game.py
@@ -3,46 +3,38 @@ import random
 
 def get_shape():
     """Asks player to enter his shape ('rock', 'paper', or 'scissors' in classic edition).
-    Player could enter '!exit' to quit the game, so function returns string.
-    :return:players decision as string or True in case when user need '!help'
+    Player could enter one of special commands, so function returns string if command not '!exit'.
+    :return:players decision as string or False in case when user wants to quit the game
     """
     shape = input()
-    check_result = check_shape(shape)
-    if check_result == 1:
+    if shape != '!exit':
         return shape
-    elif check_result == 2:
-        return False
-    elif check_result == 3:
-        # print('''Please type "rock", "paper" or "scissors"
-        #             You win if your shape beats computer's shape:
-        #             Paper > rock > scissors > paper and so on''')
-        return True
     else:
-        return True
+        print('Bye!')
+        return False
 
 
-def check_shape(shape):
+def shape_is_correct(players_shape):
     """Gets player's shape and checks that input is correct:
-    if input is one of possible shapes returns 1
-    if input ask for !help returns 2
-    if input wants to !exit out of program returns 3
-        if input is something unexpected returns 4
-    :param shape:
+    if input is one of possible shapes returns True
+    else prints required information and returns False
+    :param players_shape:
     :return:
     """
-    if shape in shape_pool:
-        return 1
-    if shape not in special_commands:
-        print('Sorry, unknown command. Possible special commands are:' + shape_pool)
-        return 4
-    if shape == '!help':
-        print('''Please type "rock", "paper" or "scissors"
-            You win if your shape beats computer's shape:
-            Paper > rock > scissors > paper and so on''')
-        return 2
-    if shape == '!exit':
-        print('Bye!')
-        return 3
+    if players_shape in shape_pool:
+        return True
+    elif players_shape in special_commands:
+        if players_shape == '!help':
+            print('''Please type "rock", "paper" or "scissors"
+                    You win if your shape beats computer's shape:
+                    Paper > rock > scissors > paper and so on''')
+            return False
+        if players_shape == '!rating':
+            print(f'Your rating: {players_scores[player_name]}')
+            return False
+    else:
+        print('Input incorrect! Possible shapes in this game is:', ', '.join(shape_pool))
+        return False
 
 
 def computers_decision(shapes):
@@ -76,28 +68,27 @@ def round_result(player, computer):
         return 'Computer'
 
 
-shape_pool = ['rock', 'paper', 'scissors']
-special_commands = ['!exit', '!help', '!rating']
+shape_pool = ['rock', 'scissors', 'paper']
+special_commands = ['!help', '!rating']
 players_scores = dict()
 
 try:
     rating_file = open('rating.txt')
     for line in rating_file:
-        players_scores.update({line.split()[0]: line.split()[1]})
+        players_scores.update({line.split()[0]: int(line.split()[1])})
 finally:
     rating_file.close()
 
 player_name = input('Enter your name: ')
 print(f'Hello, {player_name}')
+if player_name not in players_scores.keys():
+    players_scores.update({player_name: 0})
 
 while True:
     players_shape = get_shape()
-    if players_shape:
-        if players_shape not in shape_pool:
-            print('Input incorrect! Possible shapes in this game is:', ', '.join(shape_pool))
-            continue
-        computers_shape = computers_decision(shape_pool)
-        # print(computers_shape)
-        round_result(players_shape, computers_shape)
-    else:
+    print(type(players_shape))
+    if players_shape is bool and not players_shape:
         break
+    if shape_is_correct(players_shape):
+        computers_shape = computers_decision(shape_pool)
+        round_result(players_shape, computers_shape)
