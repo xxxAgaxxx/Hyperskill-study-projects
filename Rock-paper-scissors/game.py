7453dae6adf400dc31ca177437e51f8368ec753d
RomaAga <bromaaga@gmail.com>
2019-11-12 10:10:03 +0700

    Rock-Paper-Scissors Finished
    Project done, tests passed

--- Rock-paper-scissors/game.py
+++ Rock-paper-scissors/game.py
@@ -1,6 +1,23 @@
 import random
 
 
+def get_shape_pool():
+    """Asks player to choose game mode, a string of shapes
+    separated by commas without whitespaces is expected.
+    Number of shapes must be odd.
+    In case when input is empty — set classic mode with 3 shapes.
+    """
+    global shape_pool
+    shape_pool = input().split(',')
+    if len(shape_pool) % 2 == 0:
+        print('Number of possible variants must be odd in RSP-like games!')
+        return False
+    if shape_pool == ['']:
+        shape_pool = ['rock', 'paper', 'scissors']
+    return True
+
+
+
 def get_shape():
     """Asks player to enter his shape ('rock', 'paper', or 'scissors' in classic edition).
     Player could enter one of special commands, so function returns string if command not '!exit'.
@@ -23,34 +40,45 @@ def shape_is_correct(players_shape):
     """
     if players_shape in shape_pool:
         return True
-    elif players_shape in special_commands:
-        if players_shape == '!help':
-            print('''Please type "rock", "paper" or "scissors"
-                    You win if your shape beats computer's shape:
-                    Paper > rock > scissors > paper and so on''')
-            return False
-        if players_shape == '!rating':
-            print(f'Your rating: {current_scores}')
-            return False
+    if players_shape == '!help':
+        print('''Please type "rock", "paper" or "scissors"
+                You win if your shape beats computer's shape:
+                Paper > rock > scissors > paper and so on''')
+        return False
+    if players_shape == '!rating':
+        print(f'Your rating: {current_scores}')
+        return False
     else:
         print('Input incorrect! Possible shapes in this game is:', ', '.join(shape_pool))
         return False
 
 
-def computers_decision(shapes):
+def computers_decision():
     """Gets all possible shapes ('rock', 'paper', or 'scissors' in classic edition) and
     returns the value randomly selected from all possible shapes.
-    :param shapes:list of possible shapes
+    :param:
     :return:randomized value out of all possible
     """
-    random_computers_shape = random.choice(shapes)
+    random_computers_shape = random.choice(shape_pool)
     return random_computers_shape
 
 
-def round_result(player, computer):
+def round_result(player, computer, game_mode):
     """Gets two shapes as strings (two players' decisions) and resolves
      who is winner in this round. At site prints round result.
 
+    if game_mode == 5:
+        shape_pool = ['rock', 'scissors', 'lizard', 'paper', 'spock']
+        rock: ['scissors', 'lizard']
+        scissors: ['paper', 'lizard']
+        spock: ['scissors', 'rock']
+        lizard: ['paper', 'spock']
+        paper: ['spock', 'rock']
+    if game_mode == 15:
+    shape_pool = ['rock', 'gun', 'lightning', 'devil', 'dragon',
+                  'water', 'air', 'paper', 'sponge', 'wolf',
+                  'tree', 'human', 'snake', 'scissors', 'fire']
+    :param game_mode:Number of possible shapes
     :param player:Human player's decision
     :param computer:Computer's decision
     :return:Draw, or Player or computer as string
@@ -58,20 +86,27 @@ def round_result(player, computer):
     if player == computer:
         print(f'There is a draw ({player})')
         return 'Draw'
-    elif (player == 'rock' and computer == 'paper') or \
-         (player == 'paper' and computer == 'scissors') or \
-         (player == 'scissors' and computer == 'rock'):
-        print('Sorry, but computer chose ' + computer)
-        return 'Computer'
+
+    first_num = shape_pool.index(computer)
+    second_num = shape_pool.index(player)
+    if first_num < second_num:
+        if (second_num - first_num) > game_mode // 2:
+            print('Sorry, but computer chose ' + computer)
+            return 'Computer'
+        else:
+            print(f'Well done. Computer chose {computer} and failed')
+            return 'Player'
     else:
-        print(f'Well done. Computer chose {computer} and failed')
-        return 'Player'
+        if (first_num - second_num) > game_mode // 2:
+            print(f'Well done. Computer chose {computer} and failed')
+            return 'Player'
+        else:
+            print('Sorry, but computer chose ' + computer)
+            return 'Computer'
 
 
-shape_pool = ['rock', 'scissors', 'paper']
-special_commands = ['!help', '!rating']
+shape_pool = []
 players_scores = dict()
-
 with open('rating.txt', 'r') as rating_file:
     for line in rating_file:
         players_scores.update({line.split()[0]: int(line.split()[1])})
@@ -80,16 +115,17 @@ player_name = input('Enter your name: ')
 print(f'Hello, {player_name}')
 initial_scores = players_scores.get(player_name, 0)
 current_scores = initial_scores
-# if player_name not in players_scores.keys():
-#     players_scores.update({player_name: 0})
 
+while not get_shape_pool():
+    continue
+print("Okay, let's start")
 while True:
     players_shape = get_shape()
     if type(players_shape) is bool and not players_shape:
         break
     if shape_is_correct(players_shape):
-        computers_shape = computers_decision(shape_pool)
-        winner = round_result(players_shape, computers_shape)
+        computers_shape = computers_decision()
+        winner = round_result(players_shape, computers_shape, len(shape_pool))
         if winner == 'Draw':
             current_scores += 50
         if winner == 'Player':
