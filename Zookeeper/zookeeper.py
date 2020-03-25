b27b59e3c723b0398f1490b2245df1e51c35d1ce
RomaAga <bromaaga@gmail.com>
2020-03-25 23:49:43 +0700

    Stage 1

---
+++ Zookeeper/zookeeper.py
@@ -0,0 +1,12 @@
+class Zookeeper:
+
+    @staticmethod
+    def zoo_greetings():
+        print('''I do love animals!
+Start looking after animals...
+Deer looks fine.
+Bat looks happy.
+Lion looks healthy.''')
+
+
+Zookeeper.zoo_greetings()
