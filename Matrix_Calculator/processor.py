faf81bc2e55526a7d3a48c6529a05896291d8109
RomaAga <bromaaga@gmail.com>
2020-07-03 11:23:07 +0700

    Stage 1 completed

--- Matrix_Calculator/processor.py
+++ Matrix_Calculator/processor.py
@@ -1,37 +1,70 @@
 class MatrixError(Exception):
+    """ An exception class for Matrix """
     pass
 
+
 class Matrix:
-    def __init__(self, dims):
-        self.rows = [[0]*dims[0] for _ in range(dims[1])]
-        self.m = dims[0]
-        self.n = dims[1]
+    """ A simple Python matrix class with
+    basic operations and operator overloading """
+
+    def __init__(self, height, width):
+        self.rows = [[0] * width for _ in range(height)]
+        self.height = height
+        self.width = width
+
+    def __getitem__(self, line):
+        return self.rows[line]
+
+    def __setitem__(self, idx, item):
+        self.rows[idx] = item
 
     def __str__(self):
-        string = '\n'.join([' '.join([str(item) for item in row]) for row in self.rows])
-        return string + '\n'
+        s = '\n'.join([' '.join([str(item) for item in row]) for row in self.rows])
+        return s + '\n'
 
-    def getRank(self):
-        return self.m, self.n
+    def get_rank(self):
+        return self.height, self.width
 
-    def readLines(self):
-        self.rows = [map(int, input().split()) for _ in range(self.n)]
+    def __add__(self, mat):
+        """ Add a matrix to this matrix and
+        return the new matrix. Doesn't modify
+        the current matrix """
 
-    def __add__(self, other):
-        if self.getRank() != other.getRank():
+        if self.get_rank() != mat.get_rank():
             raise MatrixError
-        C = Matrix(m, n)
-        for row in range(self.rows):
-            C.rows = self.rows[row] + other.rows[row]
-        return C
+
+        ret = Matrix(self.height, self.width)
+
+        for x in range(self.height):
+            row = [sum(item) for item in zip(self.rows[x], mat[x])]
+            ret[x] = row
+
+        return ret
+
+    def __iadd__(self, mat):
+        """ Add a matrix to this matrix.
+        This modifies the current matrix """
+
+        # Calls __add__
+        tempmat = self + mat
+        self.rows = tempmat.rows[:]
+        return self
+
+    def read_matrix(self):
+        """ Read a matrix line from standard input """
+        for x in range(self.height):
+            self.rows[x] = list(map(int, input().strip().split()))
 
 
 if __name__ == '__main__':
-    A = Matrix(list(map(int, input().split())))
-    A.readLines()
-    B = Matrix(list(map(int, input().split())))
-    B.readLines()
+    rows, cols = map(int, input().split())
+    A = Matrix(rows, cols)
+    A.read_matrix()
+    rows, cols = map(int, input().split())
+    B = Matrix(rows, cols)
+    B.read_matrix()
     try:
         C = A + B
+        print(C)
     except MatrixError:
-        print('Error')
+        print('ERROR')
