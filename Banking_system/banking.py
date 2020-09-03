b63b513c329d52f190ec2dd262370513c793be83
RomaAga <bromaaga@gmail.com>
2020-09-04 01:00:15 +0700

    scratch proper error handling

--- Banking_system/banking.py
+++ Banking_system/banking.py
@@ -1,28 +1,58 @@
 import random
+import sqlite3
 
 
 class BankError(Exception):
     """An exception class for banking system"""
+    def LoginError(self):
+        pass
+
+    def FullBank(self, account):
+        pass
 
 
 class Bank:
     def __init__(self, bank_bin='400000'):
         self.__bank_bin = bank_bin
         self.__is_authorized = False
+        # будет храниться в card.number
         self.__accounts = dict()
+        self.__num_of_clients = 0
         self.__current_user = '0'
-
-    def emmit_new_card(self, card_num_len=16):
-        if len(self.__accounts) > 10 ** card_num_len - 1:
-            raise BankError('All card numbers are currently in use')
-        card_num = self.generate_card_number(card_num_len)
-        card_pin = ''.join(map(str, [random.randint(0, 9) for _ in range(4)]))
-        card_balance = 0
-        self.__accounts[card_num] = {'pin': card_pin, 'balance': card_balance}
-        print(f'''Your card has been created\nYour card number:\n{card_num}\nYour card PIN:\n{card_pin}''')
+        self.__data_base = 'example.s3db'
+        self.__db_connection = sqlite3.connect(self.__data_base)
 
     def is_logged_in(self):
-        return True if self.__is_authorized else False
+        return self.__is_authorized
+
+    def handle_user(self):
+        if not self.is_logged_in():
+            try:
+                self.handle_unlogged()
+            except BankError.LoginError:
+                print('Login or password incorrect')
+        else:
+            self.handle_client()
+
+    def handle_unlogged(self):
+        hello_message = '1. Create an account\n2. Log into account\n0. Exit\n'
+        operation = input(hello_message)
+        if operation == '1':
+            try:
+                new_account = self.emmit_new_card()
+            except BankError.FullBank():
+                print('Cant create account due to bank is overhyped')
+        elif operation == '2':
+            try:
+                self.log_in()
+            except BankError.LoginError:
+                raise BankError.LoginError
+        elif operation == '0':
+            self.__is_authorized = False
+            print('Bye!')
+        else:
+            self.__is_authorized = False
+            print('Unknown command!')
 
     def log_in(self):
         card_number = input('Enter your card number:\n')
@@ -32,7 +62,30 @@ class Bank:
             self.__current_user = card_number
             print('You have successfully logged in!')
         else:
-            print('Wrong card number or PIN!')
+            raise BankError.LoginError("Wrong card number or PIN!")
+
+    def emmit_new_card(self, card_num_len=16):
+        if self.__num_of_clients >= 10 ** (card_num_len - self.__bank_bin):
+            raise BankError.FullBank(f'All card numbers with card number length'
+                                     f' {card_num_len} in use, try to create card'
+                                     f' with larger card number length!')
+        card_num = self.generate_card_number(card_num_len)
+        card_pin = ''.join(map(str, [random.randint(0, 9) for _ in range(4)]))
+        card_balance = 0
+        self.__accounts[card_num] = {'pin': card_pin, 'balance': card_balance}
+        print(f'''Your card has been created\nYour card number:\n{card_num}\nYour card PIN:\n{card_pin}''')
+
+    def handle_client(self):
+        hello_message = '1. Balance\n2. Log out\n0. Exit\n'
+        operation = input(hello_message)
+        if operation == '1':
+            print(self.__accounts[f'{self.__current_user}']['balance'])
+        if operation == '2':
+            self.__is_authorized = False
+        if operation == '0':
+            self.__is_authorized = False
+            print('Bye!')
+        return False
 
     def user_is_valid(self, card_number, pin):
         if card_number in self.__accounts:
@@ -43,6 +96,7 @@ class Bank:
     def generate_card_number(self, num_len):
         while True:
             account_identifier = ''.join(map(str, [random.randint(0, 9) for _ in range(num_len - len(self.__bank_bin) - 1)]))
+            # @todo replase self.__accounts to (select number from table)
             if account_identifier not in self.__accounts:
                 break
         return self.__bank_bin + account_identifier + self.checksum(account_identifier)
@@ -59,42 +113,9 @@ class Bank:
         odds = sum(map(int, number_base[1::2]))
         return str((10 - (evens + odds) % 10) % 10)
 
-    def handle_unlogged(self):
-        hello_message = '1. Create an account\n2. Log into account\n0. Exit\n'
-        operation = input(hello_message)
-        if operation == '1':
-            CandleFactory.emmit_new_card()
-            return True
-        elif operation == '2':
-            CandleFactory.log_in()
-            return True
-        elif operation == '0':
-            self.__is_authorized = False
-            print('Bye!')
-        else:
-            self.__is_authorized = False
-            print('Unknown command!')
-        return False
-
-    def handle_client(self):
-        hello_message = '1. Balance\n2. Log out\n0. Exit\n'
-        operation = input(hello_message)
-        if operation == '1':
-            print(self.__accounts[f'{self.__current_user}']['balance'])
-        if operation == '2':
-            self.__is_authorized = False
-        if operation == '0':
-            self.__is_authorized = False
-            print('Bye!')
-        return False
 
 
 if __name__ == '__main__':
     CandleFactory = Bank()
     while True:
-        if not CandleFactory.is_logged_in():
-            if not CandleFactory.handle_unlogged():
-                break
-        else:
-            if not CandleFactory.handle_client():
-                break
+        CandleFactory.handle_user()
